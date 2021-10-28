21d811cff54963cee97a652590b6e0111aaecfc1
AliBinNaseer <AliIsWorking459@gmail.com>
2021-10-28 16:49:40 +0500

    About Section

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,31 +6,49 @@ export default function TextForm(props) {
 
     const upperClick = () => {
         let upperedText = text.toUpperCase();
-        setText(upperedText)
+        setText(upperedText);
     }
 
     const lowerClick = () => {
         let loweredText = text.toLowerCase();
-        setText(loweredText)
+        setText(loweredText);
+    }
+
+    const clearClick = () => {
+        setText("");
     }
 
     const changeHandle = (event) => {
         setText(event.target.value);
     }
 
+    const capClick = () => {
+        let firstChar = text.charAt(0); 
+        let newText= firstChar.toUpperCase(); 
+        setText(newText+text.slice(1)); 
+    }
+
+    let textSplit = text.split(" ").length - 1;
+
     return (
         <>
         <div>
             <h2>{props.heading}</h2>
             <div className="mb-3">
-                <textarea className="form-control" id="textBox" rows="10" value={text} onChange={changeHandle} placeholder={"Start Typing..."} ></textarea>
+                <textarea className="form-control" id="textBox" rows="7" value={text} onChange={changeHandle} placeholder={"Start Typing..."} ></textarea>
             </div>
             <button className="btn btn-primary" onClick={upperClick}>Convert To Uppercase</button>
             <button className="btn btn-primary mx-3" onClick={lowerClick}>Convert To Lowercase</button>
+            <button className="btn btn-primary " onClick={capClick}>Capitalize Text</button>
+            <button className="btn btn-danger mx-3" onClick={clearClick}>Clear Text</button>
         </div>
         <div className="container my-2">
             <h1>Text Info</h1>
-            <p>{(text.split(" ")).length} words and {text.length} characters</p>
+            <p><strong>{textSplit}</strong> words and <strong>{text.length}</strong> characters</p>
+            <p><strong>{0.008*textSplit}</strong> Minutes Read</p>
+
+            <h2>Preview</h2>
+            <p>{text}</p>
         </div>
         </>
     )
